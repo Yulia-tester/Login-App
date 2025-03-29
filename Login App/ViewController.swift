e5ab27dd7930d47ae55200da2682d9cc81c74c4a
Юлия Дегтярева <yuliia.hlavatska@gmail.com>
2025-03-29 08:47:04 -0500

    1. Removed extra UIViewController, added label 2. Added and studied: viewDidLoad() viewWillAppear(_ :) viewWillLayoutSubviews() viewDidLayoutSubviews() viewDidAppear(_:) viewWillDisappear(_:) viewDidDisappear(_:)

--- Login App/ViewController.swift
+++ Login App/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    var count = 0
+    
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("viewDidLoad")
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        count += 1
+        titleLabel.text = String(count)
+        print("viewWillAppear")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        print("viewWillLayoutSubviews")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        print("viewDidLayoutSubviews")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("viewDidLayoutSubviews")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("viewDidDisappear")
+    }
 }
 
