15b620cfddaefbe30a3172b5912c86ba97194f7b
Юлия Дегтярева <yuliia.hlavatska@gmail.com>
2025-04-03 09:39:36 -0500

    1. Rounded the corners of the Login button 2. Added a shadow to the Login button 3. Linked outlets to the View Controller, 4. Also linked Actions to the buttons

--- Login App/ViewController.swift
+++ Login App/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var envelopImageView: UIImageView!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var emailLineView: UIView!
+    @IBOutlet weak var lockImageView: UIImageView!
+    @IBOutlet weak var passswordTextField: UITextField!
+    @IBOutlet weak var passwordLineView: UIView!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var dontHaveAccountLabel: UILabel!
+    @IBOutlet weak var signupButton: UIButton!
+    
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("viewDidLoad")
+        
+        setupLoginButton()
+    }
+    
+    //  MARK: - IBActions
+    @IBAction func loginAction(_ sender: Any) {
+        performSegue(withIdentifier: "goToHomePage", sender: sender)
+    }
+    
+    @IBAction func signupAction(_ sender: Any) {
+        print("Signup")
+    }
+    
+    //  MARK: - Private methods
+    private func setupLoginButton() {
+        loginButton.layer.shadowColor = (UIColor(named: "notes") ?? UIColor.gray).cgColor
+        loginButton.layer.shadowOffset = CGSize(width: 0, height: 0)
+        loginButton.layer.shadowOpacity = 0.4
+        loginButton.layer.shadowRadius = 8
+        
     }
 }
 
